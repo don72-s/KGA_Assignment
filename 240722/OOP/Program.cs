69da9ffb31c39c6eed909b7a9f468d1bd4bad40f
김민태 <66211881+don72-s@users.noreply.github.com>
2024-07-22 15:32:09 +0900

    feat : make character class
    character 클래스 기본 형태 정의

---
+++ 240722/OOP/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP {
+    internal class Program {
+
+        class Character {
+
+            int level;
+            int hp;
+            int moveSpeed;
+            int attackPoint;
+
+            void MoveFoward() {
+
+            }
+
+            void MoveBackward() {
+
+            }
+
+            void TurnRight() {
+
+            }
+
+            void TurnLeft() {
+
+            }
+
+            void Attack() { 
+            
+            }
+
+            void Damaged() {
+
+
+            }
+
+        }
+
+        static void Main(string[] args) {
+            
+
+
+        }
+    }
+}
